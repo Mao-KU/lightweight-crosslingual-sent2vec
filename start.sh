23e066cbdc702a144b29af4821226ce5f9bb8461
Zhuoyuan Mao <55566097+Mao-KU@users.noreply.github.com>
2021-08-13 13:39:59 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-var=$4
+readonly var=$4
 la=$5
-if $var ; then
+if "$var"; then
 	CUDA_VISIBLE_DEVICES=$1 nohup python main.py -m $2 -r True -la $la >> $3/$2.out 2>&1 &
 	echo "CUDA_VISIBLE_DEVICES=$1 nohup python main$5.py -m $2 -r True -la $la >> $3/$2.out 2>&1 &"
 else
